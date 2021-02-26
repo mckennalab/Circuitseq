ea09623727605e690202bca496633ab58e45969b
femiliani <58233098+femiliani@users.noreply.github.com>
2021-02-25 22:22:34 -0500

    convert .dna to .fasta

--- scripts/file_submission/convert_dna_to_fasta.sh
+++ scripts/file_submission/convert_dna_to_fasta.sh
@@ -1,3 +1,4 @@
+#Run this in the directory with all the .dna files to convert them to fasta
 for i in *.dna ; do filename=${i%.dna}
 		    python read_dna.py $i $filename'_ref.fasta'
 		    python clean_ref.py $filename
