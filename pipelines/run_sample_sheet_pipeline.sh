d70eff2706c72a8da36f09d97096cfcb02b19636
aaronmck <aaronatwpi@gmail.com>
2021-06-22 09:43:02 -0400

    some midpoint updates

---
+++ pipelines/run_sample_sheet_pipeline.sh
@@ -0,0 +1,12 @@
+./nextflow PlasmidSeq/pipelines/plasmid_assembly_sample_sheet.nf \
+	   --samplesheet /analysis/2021_05_06_nanopore_pipeline/assess_assembly_101/fex_101_BC_to_ref_index.tsv \
+	   --barcodes /analysis/2021_05_06_nanopore_pipeline/2021_05_25_nanopore_24_plasmids/guppy_plasmid_barcodes/ \
+	   --fast5 /analysis/2021_05_06_nanopore_pipeline/fastq5_101/ \
+	   --guppy_model dna_r10.3_450bps_sup.cfg \
+	   --gpu_slot cuda:0 \
+	   --tn5proj /home/f002sd4/plasmid_seq/FEX_099/demultiplexing/barcode_tn5_base.fa.prj \
+	   --bcmat /home/f002sd4/plasmid_seq/FEX_099/demultiplexing/bc.mat \
+	   -with-report report.html \
+	   --tn5ref /home/f002sd4/plasmid_seq/FEX_099/demultiplexing/barcode_tn5_base.fa \
+	   --barcode_min_score 40 \
+	   -resume
