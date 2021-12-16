b105155b1e93a4f415b2eabbe8bec90713920105
femiliani <francesco.e.emiliani.gr@dartmouth.edu>
2021-12-16 09:41:48 -0500

    clean up the github

--- pipelines/run_pipeline.sh
+++
@@ -1,9 +0,0 @@
-./nextflow PlasmidSeq/pipelines/plasmid_assembly.nf \
-	   --fast5 ./fast5/ \
-	   --guppy_model dna_r9.4.1_450bps_hac.cfg \
-	   --gpu_slot cuda:0 \
-	   --tn5proj /home/f002sd4/plasmid_seq/FEX_099/demultiplexing/barcode_tn5_base.fa.prj \
-	   --bcmat /home/f002sd4/plasmid_seq/FEX_099/demultiplexing/bc.mat \
-	   -with-report report.html \
-	   --tn5ref /home/f002sd4/plasmid_seq/FEX_099/demultiplexing/barcode_tn5_base.fa \
-	   -resume
