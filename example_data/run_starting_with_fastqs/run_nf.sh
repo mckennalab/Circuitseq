440e3c89659d66c88f3567b276cdf74f59598a8e
Francesco Emiliani <58233098+femiliani@users.noreply.github.com>
2022-06-15 13:23:43 -0400

    Update run_nf.sh

--- example_data/run_starting_with_fastqs/run_nf.sh
+++ example_data/run_starting_with_fastqs/run_nf.sh
@@ -1,16 +1,18 @@
-NXF_VER=21.10.6 nextflow run <path to pipelines/CircuitSeq.nf> \
+#It is safest to use absolute paths  
+NXF_VER=21.10.6 nextflow run <path to /pipelines/CircuitSeq.nf> \
            --GPU ON \
-           -c <path to pipelines/nextflow.config> \
+           -c <path to /pipelines/nextflow.config> \
            -with-singularity <path to .sif file> \
            --samplesheet <path to sample_sheet.tsv> \
-           --fast5 "none" \
+           --use_existing_basecalls true \
+           --fast5 "" \
+           --basecalling_dir <path_to_fastq_dir> \
+           --base_calling_summary_file <path_to_summary.txt> \
            --barcodes /plasmidseq/barcodes/v2/ \
            --guppy_model dna_r9.4.1_450bps_sup.cfg \
            --medaka_model r941_min_sup_g507 \
            --gpu_slot cuda:0 \
            --barcode_min_score 65 \
            --quality_control_processes true \
-           --use_existing_basecalls true \
-           --basecalling_dir <path to fastq dir> \
-           --base_calling_summary_file <path_to_summary.txt> \
            -resume
+
