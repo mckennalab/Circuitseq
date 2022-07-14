b03c4f8440df3afa3f2c34d9a8bda5c0d4b208a8
Francesco Emiliani <58233098+femiliani@users.noreply.github.com>
2022-07-14 12:43:27 -0400

    Update run_nf.sh

--- pipelines/run_nf.sh
+++ pipelines/run_nf.sh
@@ -9,6 +9,7 @@ NXF_VER=21.10.6 nextflow run ./Experimental_Circuitseq/pipelines/CircuitSeq.nf \
            --basecalling_dir <path_to_fastq_dir, use "" if starting from fast5> \
            --base_calling_summary_file <path_to_summary.txt, use "" if starting from fast5> \
            --barcodes /plasmidseq/barcodes/v2/ \
+           --barcode_kit "MY-CUSTOM-BARCODES" \
            --guppy_model dna_r9.4.1_450bps_sup.cfg \
            --medaka_model r941_min_sup_g507 \
            --gpu_slot cuda:0 \
